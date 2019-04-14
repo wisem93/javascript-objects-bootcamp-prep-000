5ebb5843355a54ad473059103a4e4a88a0ea5cc2
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-04-14 15:45:59 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,12 @@ var playlist = {
   "Drake": "No Ways"
 };
 
-function updatePlaylist(playlist, "artist", "song") {
-  
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist;
+  return playlist;
 }
